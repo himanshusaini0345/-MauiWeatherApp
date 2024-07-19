d8a1976862bec4c74e15b1356d2da475a1ab0f1c
Himanshu Saini <himanshusainib.159@gmail.com>
2024-07-19 14:36:18 +0530

    Add project files.

---
+++ MauiWeatherApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiWeatherApp.Pages;
+
+namespace MauiWeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WeatherInfoPage();
+        }
+    }
+}
